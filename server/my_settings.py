ddb845247339d47fc4d10cdf967dfe7fc4f4f37c
sdc05103 <heeni1004@gmail.com>
2023-03-29 14:01:20 +0900

    change in CAFENAMU

---
+++ server/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_posts',
+        'USER': 'root',
+        'PASSWORD': '2945',
+        'HOST':'localhost',
+        'PORT': '3306',
+    }
+}
